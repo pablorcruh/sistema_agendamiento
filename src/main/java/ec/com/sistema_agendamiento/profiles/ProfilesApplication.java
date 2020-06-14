e21baa880856461d2318f8e95dae0f46808b54ef
pablorcruh <pablorcruh@gmail.com>
2020-06-14 17:42:47 -0500

    agregar  version h2

--- src/main/java/ec/com/sistema_agendamiento/profiles/ProfilesApplication.java
+++ src/main/java/ec/com/sistema_agendamiento/profiles/ProfilesApplication.java
@@ -1,13 +1,14 @@
 package ec.com.sistema_agendamiento.profiles;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+@Data
 @Configuration
 @ConfigurationProperties("spring.datasource")
-@SuppressWarnings("unused")
 public class ProfilesApplication {
 
 
