aeeebffcba778eee6115fa28e2f9f29bc98c6468
pablorcruh <pablorcruh@gmail.com>
2020-06-14 16:21:48 -0500

    initial commit

---
+++ src/main/java/ec/com/sistema_agendamiento/SistemaAgendamientoApplication.java
@@ -0,0 +1,13 @@
+package ec.com.sistema_agendamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAgendamientoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaAgendamientoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ec/com/sistema_agendamiento/SistemaAgendamientoApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.sistema_agendamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAgendamientoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
